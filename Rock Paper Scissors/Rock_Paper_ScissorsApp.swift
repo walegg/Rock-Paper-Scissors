54ef43d56c7b840b3a83cead6a6d76aaa01cef25
Louis Mille <mille.louis1@gmail.com>
2023-07-04 11:41:50 +0200

    Initial Commit

---
+++ Rock Paper Scissors/Rock_Paper_ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_ScissorsApp.swift
+//  Rock Paper Scissors
+//
+//  Created by Louis Mille on 04/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
